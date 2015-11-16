1a49e4abc569fa19ef9e9f6e9e9e7493a02f240f
Evan Hubinger <evanjhub@gmail.com>
2015-11-16 15:43:30 -0800

    Adds an index-by-slice test

---
+++ src/__coconut__.py
@@ -0,0 +1,83 @@
+# Coconut Header: --------------------------------------------------------------
+
+from __future__ import with_statement, print_function, absolute_import, unicode_literals, division
+import sys as _coconut_sys
+if _coconut_sys.version_info < (3,):
+    py2_filter, py2_hex, py2_map, py2_oct, py2_zip = filter, hex, map, oct, zip
+    from future_builtins import *
+    py2_range, range = range, xrange
+    py2_int = int
+    _coconut_int, _coconut_long = int, long
+    class _coconut_metaint(type):
+        def __instancecheck__(cls, inst):
+            return isinstance(inst, (_coconut_int, _coconut_long))
+    class int(_coconut_int):
+        """Python 3 int."""
+        __metaclass__ = _coconut_metaint
+    py2_chr, chr = chr, unichr
+    bytes, str = str, unicode
+    _coconut_encoding = "UTF-8"
+    py2_print = print
+    _coconut_print = print
+    def print(*args, **kwargs):
+        """Python 3 print."""
+        return _coconut_print(*(str(x).encode(_coconut_encoding) for x in args), **kwargs)
+    py2_input = raw_input
+    _coconut_input = raw_input
+    def input(*args, **kwargs):
+        """Python 3 input."""
+        return _coconut_input(*args, **kwargs).decode(_coconut_encoding)
+
+version = "0.3.3-post_dev"
+
+import functools
+import operator
+import itertools
+import collections
+try:
+    import collections.abc as abc
+except ImportError:
+    abc = collections
+
+object = object
+int = int
+set = set
+frozenset = frozenset
+tuple = tuple
+list = list
+len = len
+isinstance = isinstance
+getattr = getattr
+slice = slice
+ascii = ascii
+
+def recursive(func):
+    """Returns tail-call-optimized function."""
+    state = [True, None] # toplevel, (args, kwargs)
+    recurse = object()
+    @functools.wraps(func)
+    def tailed_func(*args, **kwargs):
+        """Tail Recursion Wrapper."""
+        if state[0]:
+            state[0] = False
+            try:
+                while True:
+                    result = func(*args, **kwargs)
+                    if result is recurse:
+                        args, kwargs = state[1]
+                        state[1] = None
+                    else:
+                        return result
+            finally:
+                state[0] = True
+        else:
+            state[1] = args, kwargs
+            return recurse
+    return tailed_func
+
+def datamaker(cls):
+    """Returns base data constructor."""
+    return functools.partial(super(cls, cls).__new__, cls)
+
+class MatchError(Exception):
+    """Pattern-matching error."""
